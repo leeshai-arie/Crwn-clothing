6f4606023ed3241e26c09eeb5ac75c3f78e3aba6
Leeshai Leo Arie <67233312+leeshai-arie@users.noreply.github.com>
2022-06-23 13:05:17 +0300

    created auth and sign in form

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,12 +1,12 @@
 // import { signInWithGooglePopup, createUserDocumentFromAuth,} from '../../utils/firebase/firebase.utils'; 
 import SignUpForm from '../../components/sign-up-form/sign-up-form.comonents';
 import SignInForm from '../../components/sign-in-form/sign-in-form.comonents';
+import './authentication.styles.scss'
 
 
 const Authentication = () => {
   return(
-    <div>
-      <h1>Sign In Page</h1>
+    <div className='authentication-container'>
       <SignInForm />
       <SignUpForm />
     </div>
